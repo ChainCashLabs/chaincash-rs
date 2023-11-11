0a85b6259ebe0c2b1e572fc1cf768a3000a40a0f
ross-weir <29697678+ross-weir@users.noreply.github.com>
2023-11-11 19:00:58 +1100

    tidy and remove unwraps

--- chaincash_offchain/src/transactions.rs
+++ chaincash_offchain/src/transactions.rs
@@ -1,35 +1,55 @@
 pub mod reserves;
 
+use self::reserves::{mint_reserve_transaction, MintReserveOpt};
 use crate::NanoErg;
-use ergo_lib::{
-    chain::transaction::TxId,
-    ergo_chain_types::EcPoint,
-    ergotree_ir::chain::ergo_box::ErgoBox,
-    wallet::{
-        box_selector::{BoxSelection, BoxSelector, SimpleBoxSelector},
-        tx_builder::SUGGESTED_TX_FEE,
-    },
+use ergo_lib::chain::{ergo_box::box_builder::ErgoBoxCandidateBuilderError, transaction::TxId};
+use ergo_lib::ergo_chain_types::EcPoint;
+use ergo_lib::ergotree_ir::chain::address::AddressEncoderError;
+use ergo_lib::ergotree_ir::chain::ergo_box::{box_value::BoxValueError, ErgoBox};
+use ergo_lib::ergotree_ir::chain::token::TokenAmountError;
+use ergo_lib::wallet::box_selector::{
+    BoxSelection, BoxSelector, BoxSelectorError, SimpleBoxSelector,
 };
+use ergo_lib::wallet::tx_builder::{TxBuilderError, SUGGESTED_TX_FEE};
 use ergo_node_interface::NodeInterface;
 use thiserror::Error;
-
-use self::reserves::mint_reserve_tx;
 
 #[derive(Debug, Error)]
 pub enum TransactionError {
     #[error("unable to get change address from wallet")]
     ChangeAddress,
-}
 
-#[derive(Clone)]
-pub struct TransactionService {
-    node: NodeInterface,
-    fee: NanoErg,
+    #[error("box value error: {0}")]
+    BoxValue(#[from] BoxValueError),
+
+    #[error("token value error: {0}")]
+    TokenValue(#[from] TokenAmountError),
+
+    #[error("missing box: {0}")]
+    MissingBox(String),
+
+    #[error("box builder error: {0}")]
+    BoxBuilder(#[from] ErgoBoxCandidateBuilderError),
+
+    #[error("box selection error: {0}")]
+    BoxSelection(#[from] BoxSelectorError),
+
+    #[error("tx builder error: {0}")]
+    TxBuilder(#[from] TxBuilderError),
+
+    #[error("address error: {0}")]
+    Address(#[from] AddressEncoderError),
 }
 
 pub struct TxContext {
     current_height: u32,
     change_address: String,
+    fee: NanoErg,
+}
+
+#[derive(Clone)]
+pub struct TransactionService {
+    node: NodeInterface,
     fee: NanoErg,
 }
 
@@ -47,15 +67,20 @@ impl TransactionService {
         amount: NanoErg,
     ) -> Result<BoxSelection<ErgoBox>, crate::Error> {
         let inputs = self.node.unspent_boxes_with_min_total(amount)?;
-        // kinda irrelevant since we already have suitable boxes but box selectors required by txbuilder
+        // kinda irrelevant since we already have suitable boxes but box selectors required by ergo-lib txbuilder
         Ok(SimpleBoxSelector::new()
-            .select(inputs, amount.try_into().unwrap(), &[])
-            .unwrap())
+            .select(
+                inputs,
+                amount.try_into().map_err(TransactionError::from)?,
+                &[],
+            )
+            .map_err(TransactionError::from)?)
     }
 
-    // TODO: handle wallet uninitialized/locked
     fn get_tx_ctx(&self) -> Result<TxContext, crate::Error> {
         let wallet_status = self.node.wallet_status()?;
+
+        // TODO: handle wallet uninitialized/locked
 
         Ok(TxContext {
             current_height: self.node.current_block_height()? as u32,
@@ -69,12 +94,8 @@ impl TransactionService {
     pub fn mint_reserve(&self, pk: EcPoint, amount: NanoErg) -> Result<TxId, crate::Error> {
         let ctx = self.get_tx_ctx()?;
         let selected_inputs = self.box_selection_with_amount(amount + ctx.fee)?;
-        let unsigned_tx = mint_reserve_tx(reserves::MintReserveOpt {
-            pk,
-            amount,
-            ctx,
-            inputs: selected_inputs,
-        });
+        let unsigned_tx =
+            mint_reserve_transaction(MintReserveOpt { pk, amount }, selected_inputs, ctx)?;
         Ok(self.node.sign_and_submit_transaction(&unsigned_tx)?)
     }
 }
--- chaincash_offchain/src/transactions/reserves.rs
+++ chaincash_offchain/src/transactions/reserves.rs
@@ -1,47 +1,52 @@
-use ergo_lib::{
-    chain::{
-        ergo_box::box_builder::ErgoBoxCandidateBuilder, transaction::unsigned::UnsignedTransaction,
-    },
-    ergo_chain_types::EcPoint,
-    ergotree_ir::chain::{
-        address::NetworkAddress,
-        ergo_box::{box_value::BoxValue, ErgoBox, NonMandatoryRegisterId},
-        token::{Token, TokenAmount, TokenId},
-    },
-    wallet::{box_selector::BoxSelection, tx_builder::TxBuilder},
-};
+use ergo_lib::chain::ergo_box::box_builder::ErgoBoxCandidateBuilder;
+use ergo_lib::chain::transaction::unsigned::UnsignedTransaction;
+use ergo_lib::ergo_chain_types::EcPoint;
+use ergo_lib::ergotree_ir::chain::address::NetworkAddress;
+use ergo_lib::ergotree_ir::chain::ergo_box::ErgoBox;
+use ergo_lib::ergotree_ir::chain::{ergo_box::NonMandatoryRegisterId, token::Token};
+use ergo_lib::wallet::{box_selector::BoxSelection, tx_builder::TxBuilder};
 
 use crate::contracts::RESERVE_ERGO_TREE;
 
-use super::TxContext;
+use super::{TransactionError, TxContext};
 
 pub struct MintReserveOpt {
     pub pk: EcPoint,
     pub amount: u64,
-    pub ctx: TxContext,
-    pub inputs: BoxSelection<ErgoBox>,
 }
 
-pub fn mint_reserve_tx(opts: MintReserveOpt) -> UnsignedTransaction {
+pub fn mint_reserve_transaction(
+    opts: MintReserveOpt,
+    inputs: BoxSelection<ErgoBox>,
+    context: TxContext,
+) -> Result<UnsignedTransaction, super::TransactionError> {
     let mut reserve_box_builder = ErgoBoxCandidateBuilder::new(
-        BoxValue::try_from(opts.amount).unwrap(),
+        opts.amount.try_into()?,
         RESERVE_ERGO_TREE.clone(),
-        opts.ctx.current_height,
+        context.current_height,
     );
-    reserve_box_builder.add_token(Token {
-        token_id: TokenId::from(opts.inputs.boxes.get(0).unwrap().box_id()),
-        amount: TokenAmount::try_from(1).unwrap(),
-    });
+    let nft_id = inputs
+        .boxes
+        .get(0)
+        .ok_or_else(|| {
+            TransactionError::MissingBox(
+                "mint_reserve_transaction: failed to find input box required to mint nft"
+                    .to_string(),
+            )
+        })?
+        .box_id();
+    let nft = Token {
+        token_id: nft_id.into(),
+        amount: 1.try_into()?,
+    };
+    reserve_box_builder.add_token(nft);
     reserve_box_builder.set_register_value(NonMandatoryRegisterId::R4, opts.pk.into());
-    TxBuilder::new(
-        opts.inputs,
-        vec![reserve_box_builder.build().unwrap()],
-        opts.ctx.current_height,
-        BoxValue::try_from(opts.ctx.fee).unwrap(),
-        NetworkAddress::try_from(opts.ctx.change_address)
-            .unwrap()
-            .address(),
+    Ok(TxBuilder::new(
+        inputs,
+        vec![reserve_box_builder.build()?],
+        context.current_height,
+        context.fee.try_into()?,
+        NetworkAddress::try_from(context.change_address)?.address(),
     )
-    .build()
-    .unwrap()
+    .build()?)
 }
