3acf10d103a03e446d1f99e83e458af6f22986b3
ross-weir <29697678+ross-weir@users.noreply.github.com>
2023-11-13 20:30:29 +1100

    assign contract strings to vars

--- crates/chaincash_offchain/src/contracts.rs
+++ crates/chaincash_offchain/src/contracts.rs
@@ -2,12 +2,13 @@ use ergo_lib::ergoscript_compiler::compiler::compile;
 use ergo_lib::ergotree_ir::ergo_tree::ErgoTree;
 use once_cell::sync::Lazy;
 
-pub(crate) static RESERVE_ERGO_TREE: Lazy<ErgoTree> = Lazy::new(|| {
-    let s = include_str!("../../../contracts/chaincash/contracts/onchain/reserve.es");
-    compile(s, Default::default()).unwrap()
-});
+pub(crate) static RESERVE_CONTRACT: &str =
+    include_str!("../../../contracts/chaincash/contracts/onchain/reserve.es");
+pub(crate) static NOTE_CONTRACT: &str =
+    include_str!("../../../contracts/chaincash/contracts/onchain/note.es");
 
-pub(crate) static NOTE_ERGO_TREE: Lazy<ErgoTree> = Lazy::new(|| {
-    let s = include_str!("../../../contracts/chaincash/contracts/onchain/note.es");
-    compile(s, Default::default()).unwrap()
-});
+pub(crate) static RESERVE_ERGO_TREE: Lazy<ErgoTree> =
+    Lazy::new(|| compile(RESERVE_CONTRACT, Default::default()).unwrap());
+
+pub(crate) static NOTE_ERGO_TREE: Lazy<ErgoTree> =
+    Lazy::new(|| compile(NOTE_CONTRACT, Default::default()).unwrap());
