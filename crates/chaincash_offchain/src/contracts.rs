c3ebb43c2fdc8007348c8911089434199baca29d
ross-weir <29697678+ross-weir@users.noreply.github.com>
2023-11-13 13:11:49 +1100

    move to crates dir, tidy deps

--- crates/chaincash_offchain/src/contracts.rs
+++ crates/chaincash_offchain/src/contracts.rs
@@ -3,11 +3,11 @@ use ergo_lib::ergotree_ir::ergo_tree::ErgoTree;
 use once_cell::sync::Lazy;
 
 pub(crate) static RESERVE_ERGO_TREE: Lazy<ErgoTree> = Lazy::new(|| {
-    let s = include_str!("../../contracts/chaincash/contracts/onchain/reserve.es");
+    let s = include_str!("../../../contracts/chaincash/contracts/onchain/reserve.es");
     compile(s, Default::default()).unwrap()
 });
 
 pub(crate) static NOTE_ERGO_TREE: Lazy<ErgoTree> = Lazy::new(|| {
-    let s = include_str!("../../contracts/chaincash/contracts/onchain/note.es");
+    let s = include_str!("../../../contracts/chaincash/contracts/onchain/note.es");
     compile(s, Default::default()).unwrap()
 });
